4dad989b2a9f0ec07886b21ce500bb86857d2ab5
Jan Warchoł <jan.warchol@gmail.com>
2015-03-06 00:12:58 +0100

    Change safeguards to two-letter mnemonics instead of single letters

--- .config/bash/safeguards.sh
+++ .config/bash/safeguards.sh
@@ -3,14 +3,14 @@
 # Some shell commands are dangerous - rm deletes files permamently, cp and mv
 # silently overwrite destination files if they exist. To protect myself from
 # my own mistakes I define "safe" versions of such commands.
-#
 # I could define aliases that shadow the original commands, but I think it
 # wouldn't be good to get used to a non-default behaviour.
 
-# I use trash-cli instead of rm (https://github.com/andreafrancia/trash-cli)
-alias r='trash-put'  # puts files in system trash
-alias m='\mv --backup=numbered'
-alias c='\cp --backup=numbered'
+# Move files to system trash - much safer than removing permamently with rm
+# (install trash-cli from https://github.com/andreafrancia/trash-cli)
+alias tp='trash-put'
+alias mw='\mv --backup=numbered'
+alias kp='\cp --backup=numbered'
 
 # You may want to make sure that you won't accidentally use plain rm, mv or cp
 # Bypass these aliases by prepending a slash, e.g. \rm file-without-hope
