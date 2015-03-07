6f65014427445467b689c6e697daddae757a52cc
Jan Warchoł <jan.warchol@gmail.com>
2015-03-07 14:25:17 +0100

    Let aliases work after sudo

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -22,3 +22,6 @@ alias L='less'  # typing |L is very convenient, especially using left shift
 # Show a notification when a command finishes - use like this:  sleep 10; alert
 # Taken from Ubuntu's default .bashrc.
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# let aliases work after sudo (see http://askubuntu.com/a/22043)
+alias sudo='sudo '
