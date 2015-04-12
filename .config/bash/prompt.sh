21588893c39a52719e05b990436786898ad6ec08
Jan Warchoł <jan.warchol@gmail.com>
2015-04-12 14:36:36 +0200

    Move color definitions back into prompt file
    I don't use them anywhere else, and I wanted to have all stuff related
    to the prompt in one file so that I can easily give it to anyone.
    
    Also, reword some comments and remove PS4 definition, which most probably
    isn't useful for anyone except me.

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,20 +1,35 @@
-. "$HOME/.config/bash/colors.sh"
-
 # Colored prompt stands out in the sea of text, which makes it _much_ easier
 # to navigate through the terminal output.
-# Using \[ and \] around color codes is necessary to prevent strange issues!
+
+# Define vars with color codes - may be handy for other things, too
+BOLD="\e[1m"
+DIM="\e[2m"
+NORMAL="\e[21m"
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+DEFAULT="\e[39m"
+
+RESETALL="\e[0m"
+
+# Using \[ and \] around color codes in prompt is necessary to prevent strange issues!
 pathcolor="\[${CYAN}\]"
 usercolor="\[${MAGENTA}\]"
 resetall="\[${RESETALL}\]"
 
 # When I'm logged in via ssh, display the path in scp-like format (-> easy
 # selecting with a double click) and highlight hostname with a different color.
-if [ -z "$SSH_CONNECTION" ]; then
-    hostcolor=${usercolor}
-    separator=" "
-else
+if [ -n "$SSH_CONNECTION" ]; then
     hostcolor="\[${BLUE}\]"
     separator=":"
+else
+    hostcolor=${usercolor}
+    separator=" "
 fi
 
 # $(__git_ps1) displays git repository status in the prompt, which is extremely handy.
@@ -25,4 +40,3 @@ GIT_PS1_DESCRIBE_STYLE="branch"
 GIT_PS1_SHOWUPSTREAM="verbose git"
 
 export PS1="${usercolor}\u${hostcolor}@\h${separator}${pathcolor}\w${resetall}\$(__git_ps1)\n\\$ "
-export PS4="$(tput bold)>>> $(tput sgr0)"
