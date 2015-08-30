50d8de9208ce21d3366c17ebee263ea93e4f302a
Jan Warchoł <jan.warchol@gmail.com>
2015-08-31 01:24:42 +0200

    Prompt: change colors to bright ones
    Too many color schemes have too dark colors, especially blue - it's often
    completely unreadable if used in regular, non-bright version.
    
    I'm also throwing out definitions of other colors, as defining all of them
    (including bright versions) would be bloat.

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,33 +1,23 @@
 # Colored prompt stands out in the sea of text, which makes it _much_ easier
-# to navigate through the terminal output.
+# to navigate through the terminal output.  Note that I'm using bright versions
+# of colors, because in many color palettes the normal ones are too dim.
+BR_BLUE="\e[94m"
+BR_MAGENTA="\e[95m"
+BR_CYAN="\e[96m"
+DEFAULT_COLOR="\e[0m"
 
-# Define vars with color codes - may be handy for other things, too
-BOLD="\e[1m"
-DIM="\e[2m"
-NORMAL="\e[21m"
-
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-MAGENTA="\e[35m"
-CYAN="\e[36m"
-WHITE="\e[37m"
-DEFAULT="\e[39m"
-
-RESETALL="\e[0m"
-
-# Using \[ and \] around color codes in prompt is necessary to prevent strange issues!
-pathcolor="\[${CYAN}\]"
-resetall="\[${RESETALL}\]"
+# Using \[ and \] around color codes is necessary to prevent issues
+# with the prompt such as text overlapping one line over and over again.
+pathcolor="\[${BR_CYAN}\]"
+resetcolors="\[${DEFAULT_COLOR}\]"
 
 # When I'm logged in via ssh, display the path in scp-like format (-> easy
 # selecting with a double click) and display username in a different color.
 if [ -n "$SSH_CONNECTION" ]; then
-    usercolor="\[${MAGENTA}\]"
+    usercolor="\[${BR_MAGENTA}\]"
     separator=":"
 else
-    usercolor="\[${BLUE}\]"
+    usercolor="\[${BR_BLUE}\]"
     separator=" "
 fi
 
@@ -41,4 +31,4 @@ GIT_PS1_SHOWCOLORHINTS="false"
 
 # Use PROMPT_COMMAND instead of PS1 because it supports more features of __git_ps1.
 # The syntax used here is "__git_ps1 <prefix> <suffix>", see link above.
-export PROMPT_COMMAND='__git_ps1 "${usercolor}\u@\h${pathcolor}${separator}\w${resetall}" "\n\\$ "'
+export PROMPT_COMMAND='__git_ps1 "${usercolor}\u@\h${pathcolor}${separator}\w${resetcolors}" "\n\\$ "'
